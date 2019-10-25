9a0d389507b7e7e25b5bc784fc3517b6a31aa524
JB <jb@unitesystem.com>
2019-10-25 15:00:49 -0500

    up

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -32,21 +32,25 @@
 #define S_RA LCTL(RSFT(KC_R))
 #define CTRLESC CTL_T(KC_ESC)
 #define ESCMOD LT(_MOD, KC_ESC)
+#define ALTSP LALT(KC_SPACE)
+#define ALTE LALT(KC_E)
+#define TOG TG(_MOD)
 
 enum preonic_layers {
   _QWERTY,
-  _LEADERKEY,
+  _PIPEHITTER,
   _COLEMAK,
   _DVORAK,
   _LOWER,
   _RAISE,
   _MOD,
+  _CMD,
   _ADJUST
 };
 
 enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
-  LEADERKEY,
+  PIPEHITTER,
   COLEMAK,
   DVORAK,
   LOWER,
@@ -62,15 +66,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
   ESCMOD,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  KC_LCTL,  KC_LGUI, KC_LALT, MO(_MOD),LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LCTL,  KC_LGUI, KC_LALT, MO(_CMD),LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
-[_LEADERKEY] = LAYOUT_preonic_grid( \
-  KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,  \
-  ESCMOD,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSPC, \
+[_PIPEHITTER] = LAYOUT_preonic_grid( \
+  ESCMOD,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
+  KC_BSPC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  KC_LCTL,  KC_LGUI, KC_LALT, MO(_MOD),LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  CTRLESC,  KC_LGUI, KC_LALT, MO(_CMD),LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [_COLEMAK] = LAYOUT_preonic_grid( \
@@ -78,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,  \
   ESCMOD,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  KC_LCTL,  KC_LGUI, KC_LALT, MO(_MOD),LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LCTL,  KC_LGUI, KC_LALT, MO(_CMD),LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [_DVORAK] = LAYOUT_preonic_grid( \
@@ -86,41 +90,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,     KC_BSPC,  \
   ESCMOD,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,     KC_SLSH, \
   KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,     KC_SFTENT,  \
-  KC_LCTL,  KC_LGUI, KC_LALT, MO(_MOD), LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LCTL,  KC_LGUI, KC_LALT, MO(_CMD), LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    _______,  \
-  KC_DEL,   S_AL,    S_SA,    KC_BSPC, KC_ENT,  KC_GESC,  KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, _______,  KC_BSLS, \
-  _______,  S_UN,    S_CU,    S_CO,    S_PA,    SWAPL,    SWAPR,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
+  KC_DEL,   S_AL,    S_SA,    KC_DEL,  KC_BSPC, KC_ENT,   _______,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______,  S_UN,    S_CU,    S_CO,    S_PA,    KC_ESC,   _______,  _______, _______,  _______, _______, _______, \
   _______,  _______, _______, _______, _______, _______,  _______,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,  \
-  KC_DEL,   _______, _______, _______, _______, _______,  _______,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PIPE, \
-  _______,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, SWAPL,    SWAPR,    KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
+  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
+  KC_DEL,   _______, _______, SWAPL,   SWAPR,   _______,  _______,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______,  _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______, \
   _______,  _______, _______, _______, _______, _______,  _______,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 
 [_MOD] = LAYOUT_preonic_grid( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  S_TAB,   KC_PGUP, KC_UP,   KC_PGDN, _______, _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   _______, _______,  \
-  _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY, _______, KC_P0,   KC_P4,   KC_P5,   KC_P6,   _______, _______,  \
-  _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, \
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   _______, _______,  \
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_P0,   KC_P4,   KC_P5,   KC_P6,   _______, _______,  \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT  \
+),
+
+[_CMD] = LAYOUT_preonic_grid( \
+  KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+  _______, SWAPL,   KC_UP,   SWAPR,   ALTE,    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,  \
+  _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLU, KC_MPLY, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  \
+  _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_MPRV, KC_MNXT, KC_MUTE, KC_MPLY, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
 
 [_ADJUST] = LAYOUT_preonic_grid( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   \
   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, _______,  \
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, LEADERKEY, \
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, PIPEHITTER, \
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,  _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
-
 
 };
 
@@ -132,9 +143,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
-        case LEADERKEY:
+        case PIPEHITTER:
           if (record->event.pressed) {
-            set_single_persistent_default_layer(_LEADERKEY);
+            set_single_persistent_default_layer(_PIPEHITTER);
           }
           return false;
           break;
