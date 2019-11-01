6219b3a96306878a80cb29911a296d9658f5ab7b
unites <jb@unitesystem.com>
2019-11-01 16:05:09 -0500

    save

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -114,16 +114,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE,  \
-  KC_DEL,   S_A,     S_S,     S_D,     S_F,     KC_ENT,   KC_UNDS,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_DQUO, \
-  _______,  S_Z,     S_X,     S_C,     S_V,     KC_BSPC,  _______,  _______, _______,  _______, _______, _______, \
-  _______,  _______, _______, _______, _______, KC_BSPC,  KC_BSPC,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
+  KC_DEL,   S_A,     S_S,     S_D,     S_F,     KC_ENT,   _______,  KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_DQUO, \
+  _______,  S_Z,     S_X,     S_C,     S_V,     KC_BSPC,  _______,  KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, _______, \
+  _______,  _______, _______, _______, _______, _______,  _______,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,  \
-  KC_DEL,   _______, _______, _______, _______, _______,  KC_MINS,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DQUO, \
-  _______,  _______, _______, _______, _______, _______,  KC_MNXT,   KC_MUTE, KC_LABK, KC_RABK, KC_QUES, KC_CAPS, \
+  KC_DEL,   _______, _______, _______, _______, _______,  _______,   KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_DQUO, \
+  _______,  _______, _______, _______, _______, _______,  _______,   KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_CAPS, \
   _______,  _______, _______, _______, _______, _______,  _______,   _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
 
