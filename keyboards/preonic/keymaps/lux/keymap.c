da0c7f5e5d2bbbadf780314bbc6e905b7639529e
unites <jb@unitesystem.com>
2019-11-01 15:13:55 -0500

    update pipehitter

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -41,7 +41,8 @@
 #define ESCCMD LT(_CMD, KC_ESC)
 #define L_CMD MO(_CMD)
 #define L_MOD MO(_MOD)
-#define SPACED LT(_MOD, KC_SPC)
+#define SPACEM LT(_MOD, KC_SPC)
+#define SPACEC LT(_CMD, KC_SPC)
 
 enum preonic_layers {
   _QWERTY,
@@ -79,19 +80,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_ALPHA] = LAYOUT_preonic_grid( \
-  ESCMOD,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
-  ESCCMD,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  KC_LCTL,  KC_LGUI, KC_LALT, L_MOD,   LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
-),
-
-[_PIPEHITTER] = LAYOUT_preonic_grid( \
-  ESCMOD,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
   KC_BSPC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  CTRLESC,  KC_LGUI, KC_LALT, L_CMD,   LOWER,   SPACED,  SPACED,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  CTRLESC,  KC_LGUI, KC_LALT, L_MOD,   LOWER,   SPACED,  SPACED,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+),
+
+[_PIPEHITTER] = LAYOUT_preonic_grid( \
+  KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH,  \
+  KC_BSPC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
+  CTRLESC,  KC_LGUI, KC_LALT, L_MOD,   LOWER,   SPACEC,  SPACEC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [_COLEMAK] = LAYOUT_preonic_grid( \
@@ -112,16 +113,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-  _______,  S_A,     S_S,     S_D,     S_F,     KC_ENT,   _______,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    _______,  \
+  KC_DEL,   S_A,     S_S,     S_D,     S_F,     KC_ENT,   _______,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______,  S_Z,     S_X,     S_C,     S_V,     KC_BSPC,  _______,  _______, _______,  _______, _______, _______, \
-  _______,  _______, _______, _______, _______, _______,  _______,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
+  _______,  _______, _______, _______, _______, KC_BSPC,  KC_BSPC,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  _______,  _______, _______, _______, _______, _______,  _______,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,  \
+  KC_DEL,   _______, _______, _______, _______, _______,  _______,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
   KC_CAPS,  _______, _______, _______, _______, _______,  KC_MNXT,   KC_MUTE, KC_MPLY, _______, _______, _______, \
   _______,  _______, _______, _______, _______, _______,  _______,   _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
