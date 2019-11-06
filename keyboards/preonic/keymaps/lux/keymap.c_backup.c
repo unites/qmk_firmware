70c6e41202a1f09743254fb42e565336876bf603
unites <jb@unitesystem.com>
2019-11-06 15:55:34 -0600

    update layers.

--- keyboards/preonic/keymaps/lux/keymap.c_backup.c
+++ keyboards/preonic/keymaps/lux/keymap.c_backup.c
@@ -121,18 +121,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,  \
-  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-  _______,  KC_LCBR, KC_RCBR, KC_EQL,  KC_MINS, KC_BSPC,  _______,  KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE, \
+  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
+  L_MOD,    S_GUI,   KC_DEL,  KC_BSPC, KC_ENT,  ALT_TAB,  _______,  KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_BSLS, \
+  _______,  SWAPL,   SWAPR,   _______, S_TAB,   ALTE,     _______,  KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE, \
   _______,  _______, _______, _______, _______, _______,  _______,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 
 [_RAISE] = LAYOUT_preonic_grid( \
-  KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,  \
-  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-  _______,  KC_LCBR, KC_RCBR, KC_EQL,  KC_MINS, KC_BSPC,  _______,  KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE, \
-  _______,  _______, _______, _______, _______, _______,  _______,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
+  KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+  KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
+  L_MOD,    _______, _______, _______, _______, _______,  _______,   KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_BSLS, \
+  _______,  _______, _______, _______, _______, _______,  _______,   KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE, \
+  _______,  _______, _______, _______, _______, _______,  _______,   _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
 
 [_MOD] = LAYOUT_preonic_grid( \
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   _______, SWAPL,   KC_UP,   SWAPR,   S_GUI,   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
   _______, S_GUI,   KC_DEL,  KC_BSPC, KC_ENT,  ALT_TAB, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,  _______,  \
-  _______, SWAPL,   SWAPR,   ALTE,    S_TAB,   ALTE,    KC_PSCR, KC_SLCK, KC_PAUS, _______,  _______, _______, \
+  _______, SWAPL,   SWAPR,   _______, S_TAB,   ALTE,    KC_PSCR, KC_SLCK, KC_PAUS, _______,  _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
 
