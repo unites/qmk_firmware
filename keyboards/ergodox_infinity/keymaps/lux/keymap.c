f1d67f3ec7507568cd2ee117ba6a2e3790f446fe
unites <jb@unitesystem.com>
2019-11-19 22:24:20 -0600

    tweak

--- keyboards/ergodox_infinity/keymaps/lux/keymap.c
+++ keyboards/ergodox_infinity/keymaps/lux/keymap.c
@@ -5,12 +5,16 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 #define CMD 3 // media keys
+#define GAME 4 // media keys
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   EPRM,
   VRSN,
-  RGB_SLD
+  RGB_SLD,
+  _BASE,
+  _GAME
+
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,6 +42,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
+        // left hand
+        KC_GRV,         KC_1,         KC_2,     KC_3,     KC_4,   KC_5,   KC_ESC,
+        KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,   KC_T,   KC_PGUP,
+        KC_LCTL,        KC_A,         KC_S,     KC_D,     KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,     KC_C,     KC_V,   KC_B,   KC_PGDN,
+        MO(SYMB),       MO(SYMB),     MO(SYMB), MO(SYMB), MO(SYMB),
+                                                    KC_LALT,  KC_LGUI,
+                                                              KC_HOME,
+                                               KC_SPC,KC_BSPC,KC_END,
+        // right hand
+             KC_ESC,      KC_6,   KC_7,     KC_8,   KC_9,   KC_0,             KC_DEL,
+             KC_HOME,     KC_Y,   KC_U,     KC_I,   KC_O,   KC_P,             KC_BSLS,
+                          KC_H,   KC_J,     KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
+             KC_END,      KC_N,   KC_M,     KC_COMM,KC_DOT, KC_SLSH,          KC_SFTENT,
+                                  MO(SYMB), KC_LEFT,KC_DOWN,KC_UP,            KC_RIGHT,
+             KC_RGUI,        KC_RALT,
+             KC_PGUP,
+             KC_PGDN,KC_BSPC, KC_SPC
+    ),
+[GAME] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV,         KC_1,         KC_2,     KC_3,     KC_4,   KC_5,   KC_ESC,
         KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,   KC_T,   KC_PGUP,
@@ -165,7 +189,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    // dynamically generate these.
+    // Layouts
+    case _BASE:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(BASE);
+      }
+      return false;
+      break;
+    case _GAME:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(GAME);
+      }
+      return false;
+      break;
+    // Stock
     case EPRM:
       if (record->event.pressed) {
         eeconfig_init();
