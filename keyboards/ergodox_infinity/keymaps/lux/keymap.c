ac5cdd2aa7008b69713556516eefa9f48b887bec
unites <jb@unitesystem.com>
2019-11-21 14:22:41 -0600

    final tweaks

--- keyboards/ergodox_infinity/keymaps/lux/keymap.c
+++ keyboards/ergodox_infinity/keymaps/lux/keymap.c
@@ -41,10 +41,12 @@ enum custom_keycodes {
 #define L_SWTC MO(_SWTC)
 #define L_FKEY MO(_FKEY)
 #define L_NUMP MO(_NUMP)
+#define T_NUMP TG(_NUMP)
 
 #define SWAPL LCTL(LGUI(KC_LEFT))
 #define SWAPR LCTL(LGUI(KC_RIGHT))
 #define S_GUI LGUI(KC_TAB)
+
 
 #define L_ESC LT(_MOD, KC_ESC)
 
@@ -94,20 +96,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 [_GALT] = LAYOUT_ergodox(  
         // left hand
-        KC_GRV,         KC_1,         KC_2,       KC_3,     KC_4,     KC_5,     KC_ESC,
-        KC_TAB,         KC_Q,         KC_W,       KC_E,     KC_R,     KC_T,     KC_HOME,
+        KC_GRV,         KC_1,         KC_2,       KC_3,     KC_4,     KC_5,     T_NUMP,
+        KC_TAB,         KC_Q,         KC_W,       KC_E,     KC_R,     KC_T,     KC_PGUP,
         KC_LCTL,        KC_A,         KC_S,       KC_D,     KC_F,     KC_G,
-        KC_LSFT,        KC_Z,         KC_X,       KC_C,     KC_V,     KC_B,     KC_END,
+        KC_LSFT,        KC_Z,         KC_X,       KC_C,     KC_V,     KC_B,     KC_PGDN,
         L_ESC,          L_CMD,        KC_LALT,    KC_LCTL,  SH_MON,
-                                                                      KC_DEL,   KC_ENT,
-                                                                                KC_PGUP,
-                                                            KC_SPC,   KC_BSPC,  KC_PGDN,
+                                                                      L_FKEY,   L_CMD,
+                                                                                KC_HOME,
+                                                            KC_SPC,   KC_BSPC,  KC_END,
         // right hand
-        KC_ESC,      KC_6,    KC_7,      KC_8,     KC_9,    KC_0,     KC_DEL,
-        KC_HOME,     KC_Y,    KC_U,      KC_I,     KC_O,    KC_P,     KC_BSLS,
+        T_NUMP,      KC_6,    KC_7,      KC_8,     KC_9,    KC_0,     KC_DEL,
+        KC_PGUP,     KC_Y,    KC_U,      KC_I,     KC_O,    KC_P,     KC_BSLS,
                      KC_H,    KC_J,      KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
-        KC_END,      KC_N,    KC_M,      KC_COMM,  KC_DOT,  KC_SLSH,  KC_SFTENT,
-                              L_SWTC,    KC_LEFT,  KC_DOWN, KC_UP,    KC_RIGHT,
+        KC_PGDN,     KC_N,    KC_M,      KC_COMM,  KC_DOT,  KC_SLSH,  KC_SFTENT,
+                              SH_MON,    KC_LEFT,  KC_DOWN, KC_UP,    KC_RIGHT,
         KC_RALT,     KC_RCTRL,
         KC_HOME,
         KC_END,      KC_BSPC, KC_SPC
@@ -188,10 +190,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     _______,
                             _______,    _______,    _______,
        // right hand
-       _______,   _______,    _______,  _______,    _______,    _______,  _______,
-       _______,   _______,    _______,  _______,    _______,    _______,  _______,
-                  _______,    _______,  _______,    _______,    _______,  _______,
-       _______,   _______,    _______,  _______,    _______,    _______,  _______,
+       KC_NLCK,   _______,    _______,  _______,    _______,    _______,  _______,
+       _______,   KC_PAST,    KC_P7,    KC_P8,      KC_P9,      KC_PPLS,  _______,
+                  KC_PSLS,    KC_P4,    KC_P5,      KC_P6,      KC_PMNS,  _______,
+       _______,   KC_P0,      KC_P1,    KC_P2,      KC_P3,      KC_PENT,  _______,
                               _______,  _______,    _______,    _______,  _______,
        _______,   _______,
        _______, 
@@ -222,7 +224,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MOD] = LAYOUT_ergodox(
        // left hand
       RESET,     BASE,      GALT,       _______,    _______,    _______,  _______,
-      _______,   BL_INC,    BL_ON,      _______,     _______,    _______,  _______,
+      DEBUG,     BL_INC,    BL_ON,      _______,     _______,    _______,  _______,
       _______,   BL_DEC,    BL_OFF,    _______,    _______,    _______,
       _______,   BL_TOGG,   _______,    _______,    _______,    _______,  _______,
       _______,   _______,   _______,    _______,    _______,  
