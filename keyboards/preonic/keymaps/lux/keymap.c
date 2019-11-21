0ec2f2d0aad6b072b5c40111eacd4398cd02f6c6
unites <jb@unitesystem.com>
2019-11-21 15:34:56 -0600

    swap layer for preonic
    swap hands is broken in this fersion of qmk

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -62,7 +62,8 @@ enum preonic_layers {
   _RAISE,
   _MOD,
   _CMD,
-  _ADJUST
+  _ADJUST,
+  _SWAP
 };
 
 enum preonic_keycodes {
@@ -75,7 +76,8 @@ enum preonic_keycodes {
   RAISE,
   MOD,
   BACKLIT,
-  ALT_TAB
+  ALT_TAB,
+  SWAP
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -101,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  ESCMOD,   L_CMD,   KC_LALT, KC_LCTL, SH_MON,  KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  ESCMOD,   L_CMD,   KC_LALT, KC_LCTL, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [_COLEMAK] = LAYOUT_preonic_grid( \
@@ -118,6 +120,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   L_MOD,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,     KC_SLSH, \
   KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,     KC_SFTENT,  \
   ESCMOD,   KC_LGUI, KC_LALT, KC_LCTL, LOWER,  KC_SPC,  KC_SPC,  RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+),
+
+[_SWAP] = LAYOUT_preonic_grid( \
+  KC_DEL,   KC_0,    KC_9,    KC_8,    KC_7,    KC_6,    KC_5,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  KC_BSPC,  KC_P,    KC_O,    KC_I,    KC_U,    KC_Y,    KC_T,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
+  KC_QUOT,  KC_SCLN, KC_L,    KC_K,    KC_J,    KC_H,    KC_G,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_SFTENT,KC_SLSH, KC_DOT,  KC_COMM, KC_M,    KC_N,    KC_B,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
+  ESCMOD,   L_CMD,   KC_LALT, KC_LCTL, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [_LOWER] = LAYOUT_preonic_grid( \
--- keyboards/preonic/keymaps/lux/rules.mk
+++ keyboards/preonic/keymaps/lux/rules.mk
@@ -2,4 +2,4 @@ SRC += muse.c
 BACKLIGHT_ENABLE  = no
 # COMBO_ENABLE = yes
 # LEADER_ENABLE = yes
-SWAP_HANDS_ENABLE = yes
+# SWAP_HANDS_ENABLE = yes
