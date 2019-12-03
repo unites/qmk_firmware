3442b9bfa2fde071f720a1acc1e372f9bd907d2c
unites <jb@unitesystem.com>
2019-12-03 14:50:17 -0600

    updated gaming layer and order

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -108,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAMER] = LAYOUT_preonic_grid( \
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
-  L_SWAP,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  ESCMOD,   L_CMD,   KC_LALT, KC_LCTL, LOWER,  KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  ESCMOD,   L_SWAP,  KC_LGUI, KC_LALT LOWER,  KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 [_WINOS] = LAYOUT_preonic_grid( \
@@ -162,11 +162,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_MOD] = LAYOUT_preonic_grid( \
-  _______,  BASE,    GAMER,   WINOS,    MACOS,   BACKSP,  COLEMAK, _______, _______, _______, _______, _______, \
+  _______,  BASE,    WINOS,   MACOS,    GAMER,   BACKSP,  COLEMAK, _______, _______, _______, _______, _______, \
   _______,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_NLCK, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_BSPC,  \
   _______,  KC_F5,   KC_F6,   KC_F7,    KC_F8,   _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, KC_NUBS,  \
   _______,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   _______, KC_PENT, KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_ENT, \
-  _______,  _______, _______, _______,  _______, _______, _______, KC_P0,   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT  \
+  _______,  _______, _______, _______,  _______, _______, _______, KC_P0,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  \
 ),
 
 [_CMD] = LAYOUT_preonic_grid( \
@@ -180,7 +180,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_preonic_grid( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   \
   _______, RESET,   _______, _______, _______, _______, _______, TERM_ON, TERM_OFF,COLEMAK, _______, BACKSP,  \
-  _______, DEBUG,   MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, BASE,    GAMER,   WINOS,   MACOS,   _______, \
+  _______, DEBUG,   MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, BASE,    WINOS,   MACOS,   GAMER,   _______, \
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,  _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
