8b4f3049bc99f5259afce5f9162ebebf91997901
unites <jb@unitesystem.com>
2019-12-07 23:53:26 -0600

    preonic update

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BACKSP] = LAYOUT_preonic_grid( \
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
   KC_BSPC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
   ESCMOD,   KC_LGUI, KC_LALT, KC_LCTL, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_MUTE, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
   _______, KC_VOLD, KC_BSPC, KC_DEL,  KC_ENT,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  \
-  _______, KC_LCBR, KC_RCBR, KC_EQL,  KC_MINS, KC_BSPC, KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,  _______, _______, \
+  _______, KC_LCBR, KC_RCBR, KC_EQL,  KC_MINS, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,  _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
 
@@ -162,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_MOD] = LAYOUT_preonic_grid( \
-  _______,  BASE,    WINOS,   GAMER,    MACOS,   BACKSP,  COLEMAK, _______, _______, _______, _______, _______, \
+  KC_ESC,   BASE,    WINOS,   GAMER,    MACOS,   BACKSP,  COLEMAK, _______, _______, _______, _______, _______, \
   _______,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_NLCK, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_BSPC,  \
   _______,  KC_F5,   KC_F6,   KC_F7,    KC_F8,   _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, KC_NUBS,  \
   _______,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   _______, KC_PENT, KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_ENT, \
