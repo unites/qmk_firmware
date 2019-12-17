8fd24c5d8b3bda9c79b9b4503e3ae1fe6293206c
unites <jb@unitesystem.com>
2019-12-17 14:00:18 -0600

    updt preonic

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -136,9 +136,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_ESC,  KC_MPLY, KC_MPRV, KC_MNXT, _______, KC_SLCK, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
+  KC_ESC,  KC_MPLY, KC_MPRV, KC_MNXT, MOVEL,   MOVER,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
   KC_DEL,  KC_MUTE, KC_VOLD, KC_VOLU, KC_ENT,  KC_PSCR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  \
-  _______, KC_LCBR, KC_RCBR, KC_EQL,  KC_MINS, KC_PAUS, _______, _______, _______, _______, KC_MPLY, _______, \
+  _______, KC_LCBR, KC_RCBR, KC_EQL,  KC_MINS, KC_PAUS, KC_SLCK, _______, _______, _______, KC_MPLY, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
 
@@ -152,9 +152,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_GTIME] = LAYOUT_preonic_grid(
   KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_SLCK,  KC_HOME,  KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    _______,  KC_HOME,  KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
   KC_DEL,   KC_5,    KC_6,    KC_7,    KC_8,    KC_PSCR,  KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  \
-  _______,  KC_9,    KC_0,    KC_EQL,  KC_MINS, KC_PAUS,  _______,  _______, _______, _______, KC_MPLY, _______, \
+  _______,  KC_9,    KC_0,    KC_EQL,  KC_MINS, KC_PAUS,  KC_SLCK,  _______, _______, _______, KC_MPLY, _______, \
   _______,  _______, _______, _______, _______, _______,  _______,  _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
 
