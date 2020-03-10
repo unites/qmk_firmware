7d1848ba9285e618f5c496a2d34b8f57784d4f3d
unites <jb@unitesystem.com>
2020-03-10 16:19:26 -0500

    final

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -88,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_ESC,  KC_LCBR, KC_RCBR, KC_ENT,  KC_BSPC, KC_DEL,  KC_DEL,  KC_BSPC, KC_ENT,  KC_LCBR, KC_RCBR, _______,  \
-  _______, KC_LPRN, KC_RPRN, KC_EQL,  KC_MINS, KC_PSCR, _______, KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, _______,  \
-  _______, KC_LBRC, KC_RBRC, KC_PLUS, KC_UNDS, KC_PAUS, _______, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, _______, \
+  KC_ESC,  KC_LCBR, KC_RCBR, KC_ENT,  KC_BSPC, KC_DEL,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
+  _______, KC_LPRN, KC_RPRN, KC_EQL,  KC_MINS, KC_PSCR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  \
+  _______, KC_LBRC, KC_RBRC, KC_PLUS, KC_UNDS, KC_PAUS, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_CMD] = LAYOUT_preonic_grid(
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  _______, KC_WBAK, KC_WFWD, KC_ENT,  KC_BSPC,  KC_DEL,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
+  _______, KC_WBAK, KC_WFWD, KC_ENT,  KC_BSPC,  KC_DEL, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  _______,  \
   _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  \
   _______, KC_MYCM, KC_MPRV, KC_MNXT, KC_WFAV, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
