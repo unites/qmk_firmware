72d127f3b6a77f8d3a00acbeecb994e759a36901
JB <jb@unitesystem.com>
2020-07-02 11:49:14 -0500

    updated brackets, more ergo

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -99,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, \
   KC_GRV,  KC_LPRN, KC_RPRN, KC_ENT,  KC_BSPC, KC_DEL,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  KC_BSLS,  \
-  KC_TILD, KC_LCBR, KC_RCBR, TAB_F,   KC_MINS, KC_PLUS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_PIPE,  \
-  _______, KC_LBRC, KC_RBRC, TAB_B,   KC_UNDS, KC_EQL,  _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, \
+  KC_TILD, KC_LBRC, KC_RBRC, TAB_F,   KC_MINS, KC_PLUS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_PIPE,  \
+  _______, KC_LCBR, KC_RCBR, TAB_B,   KC_UNDS, KC_EQL,  _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 
