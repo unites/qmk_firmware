cd694f84bef66afb1f8a66b20869ffe92ef4c7fd
JB <jb@unitesystem.com>
2020-11-09 13:10:12 -0600

    added fkey layer

--- keyboards/preonic/keymaps/lux/keymap.c
+++ keyboards/preonic/keymaps/lux/keymap.c
@@ -69,6 +69,7 @@ uint16_t alt_tab_timer = 0;
 
 enum preonic_layers {
   _BASE,
+  _FKEY,
   _COLEMAK,
   _LOWER,
   _RAISE,
@@ -79,6 +80,7 @@ enum preonic_layers {
 
 enum preonic_keycodes {
   BASE = SAFE_RANGE,
+  FKEY,
   COLEMAK,
   LOWER,
   RAISE,
@@ -106,6 +108,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT_preonic_grid( \
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
+  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
+  KC_ESC,   L_CMD,   KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+),
+
+[_FKEY] = LAYOUT_preonic_grid( \
+  KC_F11,  KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, \
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
@@ -156,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   RESET,   DEBUG,   AG_SWAP, AG_NORM,  TERM_ON, TERM_OFF, KC_F6,   AU_ON,   AU_OFF,  MU_ON,   MU_OFF,  MU_TOG,   \
   KC_F13,  KC_F14,  KC_F15,  KC_F16,   KC_F17,  KC_F18,   KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  \
   KC_CAPS, DM_REC1, DM_RSTP, DM_PLY1,  DM_PLY2, _______, _______, _______, _______,  _______, _______, _______, \
-  _______, DM_REC2, DM_RSTP, COLEMAK,  CMD,     BASE,    _______, _______, _______,  _______, _______, _______, \
+  _______, DM_REC2, DM_RSTP, COLEMAK,  CMD,     BASE,    FKEY,    _______, _______,  _______, _______, _______, \
   _______, _______, _______, _______,  _______, _______, _______, _______, _______,  _______, _______, _______  \
 )
 
@@ -167,6 +177,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case BASE:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_BASE);
+          }
+          return false;
+          break;
+        case FKEY:
+          if (record->event.pressed) {
+            set_single_persistent_default_layer(_FKEY);
           }
           return false;
           break;
